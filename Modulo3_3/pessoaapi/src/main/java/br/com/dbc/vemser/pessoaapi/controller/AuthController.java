ce89ad079d407a2c120cfe454e6d81115e279f99
flavio <flaviosguian@gmail.com>
2022-04-01 10:49:57 -0300

    fix homework

--- Modulo3_3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/AuthController.java
+++ Modulo3_3/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/AuthController.java
@@ -41,8 +41,7 @@ public class AuthController {
     }
 
     @PostMapping("/cadastrar-novo-usuario")
-    public LoginDTO create(@Valid @RequestBody CadastroUsuarioDTO newUsuario,
-                           @RequestParam("idGrupo")Integer idGrupo) throws Exception {
+    public LoginDTO create(@Valid @RequestBody CadastroUsuarioDTO newUsuario) throws Exception {
         return usuarioService.create(newUsuario);
     }
 
