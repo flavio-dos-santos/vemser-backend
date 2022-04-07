b7447c00fcb2ab79eb45aa8e3aba10d6f05b417c
flavio <flaviosguian@gmail.com>
2022-04-07 11:57:40 -0300

    fix: homework

--- modulo4/aula3/MongoJava/src/main/java/Main.java
+++ modulo4/aula3/MongoJava/src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String uri = "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+        String uri = "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&directConnection=true&ssl=false";
         MongoClient mongoClient = MongoClients.create(uri);
 
         MongoDatabase mongoDatabase = mongoClient.getDatabase("vemserpesadelonacozinha");
@@ -24,13 +24,16 @@ public class Main {
         MongoCollection<Document> receitas = mongoDatabase.getCollection("receitas");
         MongoCollection<Document> usuarios = mongoDatabase.getCollection("usuarios");
 
+
+
         // Nova receita
         Document novaReceita = new Document("recipeName", "Mastruz com leite")
                 .append("prepareRecipe", "Mastruz e leite")
                 .append("prepareTime", 10)
                 .append("ingredientes", Arrays.asList("mastruz", "leite"))
                 .append("price", 10)
-                .append("calories", 200);
+                .append("calories", 200)
+                .append("username","flavio");
 
         Document novaReceita1 = new Document("recipeName", "arroz")
                 .append("prepareRecipe", "uashusah uajsuidhaushdasjdui")
@@ -39,8 +42,11 @@ public class Main {
                 .append("price", 12.30)
                 .append("calories", 318);
 
+
+
+
         //  POST
-//         receitas.insertOne(novaReceita);
+//        System.out.println(receitas.insertOne(novaReceita));
 //         receitas.insertOne(novaReceita1);
 
 
@@ -48,8 +54,34 @@ public class Main {
 //         usuarios.updateOne(Filters.eq("username", "flavio"), ("$set","email","flavio@gmail.com");
 
 
+//        Document encontrado = receitas.find(new Document("recipeName","Mastruz com leite")).first();
+//
+//        if (encontrado != null){
+//            System.out.println("receita encontrada!");
+//
+//            Bson updateValue = new Document("calories",288);
+//            Bson updateObject = new Document("$set",updateValue);
+//            receitas.updateOne(encontrado,updateObject);
+//            System.out.println("receita atualizada!");
+//        }
+//
+
+
+//        Document usuarioEncontrado = usuarios.find(new Document("username","flavio")).first();
+//
+//        if (usuarioEncontrado != null){
+//            System.out.println("usuario encontrada!");
+//
+//            Bson updateValue = new Document("username","marcos");
+//            Bson updateObject = new Document("$set",updateValue);
+//            receitas.updateOne(usuarioEncontrado,updateObject);
+//            System.out.println("usuario atualizado!");
+//        }
+
+
+
         //delete
-        receitas.deleteOne(Filters.eq("recipeName" , "Mastruz com leite"));
+//        receitas.deleteOne(Filters.eq("recipeName" , "Mastruz com leite"));
 //        receitas.deleteOne(Filters.eq("calories" , "200"));
 
 
@@ -63,7 +95,7 @@ public class Main {
         System.out.println("-- Receitas");
         receitas.aggregate(
                 Arrays.asList(
-                        match(Filters.eq("calories", 1000)),
+                        match(Filters.eq("calories",new Document("$lte",1000))),
                         group("$username", Accumulators.sum("calories", "$calories"))
                 )).forEach(doc -> System.out.println(doc.toJson()));
 
